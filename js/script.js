61e2c6be3b82e6afc78a1582fb007a63422b83b1
Angelo7x <abennici96@gmail.com>
2021-09-20 23:35:31 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const app = new Vue ({
+    el: "#root", 
+    data: {
+        message: "Forza Milan!",
+        image: "https://tmw-storage.tcccdn.com/storage/tuttomercatoweb.com/img_notizie/thumb3/c1/c1473147f2cefdc515925cd28fcf19b0-42658-95723157c8ebcac4750156d2b21ac04e.jpeg"
+    },
+});
